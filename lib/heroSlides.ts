6b561c40bf00071ee524b642dd844ca988c9d19a
Aman Sharma <sharmaa1518@gmail.com>
2025-12-13 21:17:23 +0530

    Redsigned the Hero section

---
+++ lib/heroSlides.ts
@@ -0,0 +1,35 @@
+// heroSlides.ts
+export const heroSlides = [
+  {
+    title: "Burger",
+    heading: ["Everything", "is better", "with a"],
+    description:
+      "Burger is the missing piece that makes every day complete, a simple yet delicious joy in life",
+    image: "/images/burger.png",
+    imagePosition: "10% 110%",
+  },
+  {
+    title: "Pizza",
+    heading: ["Happiness", "is a slice", "of"],
+    description:
+      "Hot, cheesy, and loaded with flavors that bring instant comfort.",
+    image: "/images/pizza2.png",
+    imagePosition: "10% 110%",
+  },
+  {
+    title: "Milkshake",
+    heading: ["Life", "is sweeter", "with a"],
+    description:
+      "Rich, creamy shakes that turn moments into memories.",
+    image: "/images/chocolateShake.png",
+    imagePosition: "10% 110%",
+  },
+  {
+    title: "Sushi",
+    heading: ["Freshness", "served", "as"],
+    description:
+      "Delicate, precise, and crafted to perfection.",
+    image: "/images/sushi1.png",
+    imagePosition: "10% 110%",
+  },
+]
